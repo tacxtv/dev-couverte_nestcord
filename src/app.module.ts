28e5b8db9acaf53dc85c8421eeb203922ea6dc1b
Tacx <12997062+tacxou@users.noreply.github.com>
2024-08-25 17:02:06 +0200

    chore: Update app.module.ts for configuration loading and provider addition

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from "@nestjs/common"
 import { AppService } from "./app.service"
 import { NecordModule } from "necord"
 import { ConfigModule, ConfigService } from "@nestjs/config"
+import config from "./config"
+import { AppUpdate } from "./app.update"
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [config],
     }),
     NecordModule.forRootAsync({
       imports: [ConfigModule],
@@ -16,7 +19,10 @@ import { ConfigModule, ConfigService } from "@nestjs/config"
       }),
     }),
   ],
-  providers: [AppService],
+  providers: [
+    AppService,
+    AppUpdate,
+  ],
 })
 export class AppModule {
 }
