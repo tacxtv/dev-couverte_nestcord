82ca83d504ab44867a4b4f8041c9aa1dc9b481a7
Tacx <12997062+tacxou@users.noreply.github.com>
2024-08-25 16:47:05 +0200

    chore: Update main.ts for NestJS application initialization and hot module replacement

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+
+declare const module: any;
 (async () => {
-    console.log('Hello World')
+  const app = await NestFactory.createApplicationContext(AppModule)
+  app.enableShutdownHooks()
+
+  if (module.hot) {
+    module.hot.accept()
+    module.hot.dispose(() => app.close())
+  }
 })()
